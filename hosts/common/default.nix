e00cfad4bf9e5664f60ff7a6dceebe55a5634b60
Junxuan Liao <ljx@cs.wisc.edu>
2026-02-09 16:09:33 -0600

    set dockerCompat to false for podman

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -13,7 +13,7 @@
   virtualisation = {
     podman = {
       enable = true;
-      dockerCompat = true;
+      dockerCompat = false;
       defaultNetwork.settings.dns_enabled = true;
     };
   };
